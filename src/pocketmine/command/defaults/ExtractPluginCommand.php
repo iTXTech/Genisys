8a95bb97dd55713e5f17e6ea2037a20af7b86125
Hsinweic Chen <edwinyoo44@users.noreply.github.com>
2015-12-31 23:06:06 +0800

    Update ExtractPluginCommand.php

--- src/pocketmine/command/defaults/ExtractPluginCommand.php
+++ src/pocketmine/command/defaults/ExtractPluginCommand.php
@@ -40,7 +40,7 @@ class ExtractPluginCommand extends VanillaCommand{
 			return true;
 		}
 
-		$folderPath = Server::getInstance()->getPluginPath().DIRECTORY_SEPARATOR . "iTX-Genisys" . DIRECTORY_SEPARATOR . $description->getName()."_v".$description->getVersion()."/";
+		$folderPath = Server::getInstance()->getPluginPath().DIRECTORY_SEPARATOR . "PocketMine-iTX" . DIRECTORY_SEPARATOR . $description->getName()."_v".$description->getVersion()."/";
 		if(file_exists($folderPath)){
 			$sender->sendMessage("Plugin already exists, overwriting...");
 		}else{
