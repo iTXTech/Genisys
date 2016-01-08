27463070910ce74d842f3448f02d2db518d485dd
PeratX <1215714524@qq.com>
2016-01-08 21:52:42 +0800

    Thanks @aodzip

--- src/pocketmine/utils/MainLogger.php
+++ src/pocketmine/utils/MainLogger.php
@@ -215,7 +215,7 @@ class MainLogger extends \AttachableThreadedLogger{
 		}
 	}
 
-	public function run(){
+	/*public function run(){
 		$this->shutdown = false;
 		if($this->write){
 			$this->logResource = fopen($this->logFile, "a+b");
@@ -246,6 +246,33 @@ class MainLogger extends \AttachableThreadedLogger{
 			}
 
 			fclose($this->logResource);
+		}
+	}*/
+
+	public function run(){
+		$this->shutdown = false;
+		if($this->write){
+			$this->logResource = file_put_contents($this->logFile, "a+b", FILE_APPEND);
+
+			while($this->shutdown === false){
+				if(!$this->write) break;
+				$this->synchronized(function (){
+					while($this->logStream->count() > 0){
+						$chunk = $this->logStream->shift();
+						fwrite($this->logResource, $chunk);
+						$this->logResource = file_put_contents($this->logFile, $chunk, FILE_APPEND);
+					}
+
+					$this->wait(25000);
+				});
+			}
+
+			if($this->logStream->count() > 0){
+				while($this->logStream->count() > 0){
+					$chunk = $this->logStream->shift();
+					$this->logResource = file_put_contents($this->logFile, $chunk, FILE_APPEND);
+				}
+			}
 		}
 	}
 
