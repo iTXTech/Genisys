f0aadafc955fc4490f3037d9df82ef127a6e65b8
PeratX <1215714524@qq.com>
2016-01-17 19:42:23 +0800

    Small fix

--- src/pocketmine/level/format/mcregion/Chunk.php
+++ src/pocketmine/level/format/mcregion/Chunk.php
@@ -37,7 +37,7 @@ use pocketmine\utils\BinaryStream;
 
 class Chunk extends BaseFullChunk{
 
-	/** @var Compound */
+	/** @var CompoundTag */
 	protected $nbt;
 
 	public function __construct($level, CompoundTag $nbt = null){
