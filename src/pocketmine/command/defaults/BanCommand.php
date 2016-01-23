41d9377bf4c44ab6e444131c082b1bfca3f37564
labi <1292504237@qq.com>
2016-01-23 11:53:30 +0800

    Added timeban./ban <Player> [reason] [time]

--- src/pocketmine/command/defaults/BanCommand.php
+++ src/pocketmine/command/defaults/BanCommand.php
@@ -27,39 +27,51 @@ use pocketmine\event\TranslationContainer;
 use pocketmine\Player;
 
 
-class BanCommand extends VanillaCommand{
+class BanCommand extends VanillaCommand
+{
 
-	public function __construct($name){
-		parent::__construct(
-			$name,
-			"%pocketmine.command.ban.player.description",
-			"%commands.ban.usage"
-		);
-		$this->setPermission("pocketmine.command.ban.player");
-	}
+    public function __construct($name)
+    {
+        parent::__construct(
+            $name,
+            "%pocketmine.command.ban.player.description",
+            "%commands.ban.usage"
+        );
+        $this->setPermission("pocketmine.command.ban.player");
+    }
 
-	public function execute(CommandSender $sender, $currentAlias, array $args){
-		if(!$this->testPermission($sender)){
-			return true;
-		}
+    public function execute(CommandSender $sender, $currentAlias, array $args)
+    {
+        if (!$this->testPermission($sender)) {
+            return true;
+        }
 
-		if(count($args) === 0){
-			$sender->sendMessage(new TranslationContainer("commands.generic.usage", [$this->usageMessage]));
+        if (count($args) === 0) {
+            $sender->sendMessage(new TranslationContainer("commands.generic.usage", [$this->usageMessage]));
 
-			return false;
-		}
+            return false;
+        }
 
-		$name = array_shift($args);
-		$reason = implode(" ", $args);
+        $name = array_shift($args);
+        if (isset($args[0]) and isset($args[1])) {
+            $reason = $args[0];
+            if ($args[1] != null and is_numeric($args[1])) {
+                $until = $args[1] * 86400 + time();
+            } else {
+                $until = null;
+            }
 
-		$sender->getServer()->getNameBans()->addBan($name, $reason, null, $sender->getName());
+            $sender->getServer()->getNameBans()->addBan($name, $reason, $until, $sender->getName());
+        }
+        $sender->getServer()->getNameBans()->addBan($name, $reason = implode(" ", $args), null, $sender->getName());
 
-		if(($player = $sender->getServer()->getPlayerExact($name)) instanceof Player){
-			$player->kick($reason !== "" ? "Banned by admin. Reason: " . $reason : "Banned by admin.");
-		}
 
-		Command::broadcastCommandMessage($sender, new TranslationContainer("%commands.ban.success", [$player !== null ? $player->getName() : $name]));
+        if (($player = $sender->getServer()->getPlayerExact($name)) instanceof Player) {
+            $player->kick($reason !== "" ? "Banned by admin. Reason: " . $reason : "Banned by admin." . "Banned Until:" . date('r', $until="Forever"));
+        }
 
-		return true;
-	}
+        Command::broadcastCommandMessage($sender, new TranslationContainer("%commands.ban.success", [$player !== null ? $player->getName() : $name]));
+
+        return true;
+    }
 }
--- src/pocketmine/permission/BanEntry.php
+++ src/pocketmine/permission/BanEntry.php
@@ -69,7 +69,7 @@ class BanEntry{
 	}
 
 	public function hasExpired(){
-		$now = new \DateTime();
+		$now = time();
 
 		return $this->expirationDate === null ? false : $this->expirationDate < $now;
 	}
@@ -90,7 +90,7 @@ class BanEntry{
 		$str .= "|";
 		$str .= $this->getSource();
 		$str .= "|";
-		$str .= $this->getExpires() === null ? "Forever" : $this->getExpires()->format(self::$format);
+		$str .= $this->getExpires() === null ? "Forever" : $this->getExpires();
 		$str .= "|";
 		$str .= $this->getReason();
 
@@ -115,7 +115,7 @@ class BanEntry{
 					if(count($str) > 0){
 						$expire = trim(array_shift($str));
 						if(strtolower($expire) !== "forever" and strlen($expire) > 0){
-							$entry->setExpires(\DateTime::createFromFormat(self::$format, $expire));
+							$entry->setExpires($expire);
 						}
 						if(count($str) > 0){
 							$entry->setReason(trim(array_shift($str)));
