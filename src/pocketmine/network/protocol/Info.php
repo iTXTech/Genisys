c3354119b7a0de51d557d4d9ce94505fd7c07085
PeratX <1215714524@qq.com>
2016-01-26 14:42:13 +0800

    LoginPacket

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -30,9 +30,9 @@ interface Info{
 	/**
 	 * Actual Minecraft: PE protocol version
 	 */
-	const CURRENT_PROTOCOL = 38;
+	const CURRENT_PROTOCOL = 41;
 
-	const LOGIN_PACKET = 0x8f;
+	const LOGIN_PACKET = 0x8e;
 	const PLAY_STATUS_PACKET = 0x90;
 	const DISCONNECT_PACKET = 0x91;
 	const BATCH_PACKET = 0x92;
