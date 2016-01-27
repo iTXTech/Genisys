c790f360ec413155c00173a3c21bf40d586c90d5
PeratX <1215714524@qq.com>
2016-01-27 08:50:05 +0800

    PHP5 0.14 support

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -61,11 +61,11 @@ namespace pocketmine {
 	use pocketmine\utils\Utils;
 	use pocketmine\wizard\Installer;
 
-	const VERSION = "1.0dev-legacy";
+	const VERSION = "1.1dev-legacy";
 	const API_VERSION = "1.13.1";
-	const CODENAME = "Hello, 2016 !";
-	const MINECRAFT_VERSION = "v0.13.1 alpha";
-	const MINECRAFT_VERSION_NETWORK = "0.13.1";
+	const CODENAME = "0.14.0!";
+	const MINECRAFT_VERSION = "v0.14.0 alpha build 1";
+	const MINECRAFT_VERSION_NETWORK = "0.14.0";
 	const iTX_API_VERSION = '1.5.8';
 
 	/*
--- src/pocketmine/network/RakLibInterface.php
+++ src/pocketmine/network/RakLibInterface.php
@@ -215,10 +215,10 @@ class RakLibInterface implements ServerInstance, AdvancedSourceInterface{
 				if(!isset($packet->__encapsulatedPacket)){
 					$packet->__encapsulatedPacket = new CachedEncapsulatedPacket;
 					$packet->__encapsulatedPacket->identifierACK = null;
-					$packet->__encapsulatedPacket->buffer = $packet->buffer;
+					$packet->__encapsulatedPacket->buffer =  chr("0x8e") . $packet->buffer;
 					if($packet->getChannel() !== 0){
 						$packet->__encapsulatedPacket->reliability = 3;
-						$packet->__encapsulatedPacket->orderChannel = $packet->getChannel();
+						$packet->__encapsulatedPacket->orderChannel = chr("0x8e") . $packet->getChannel();
 						$packet->__encapsulatedPacket->orderIndex = 0;
 					}else{
 						$packet->__encapsulatedPacket->reliability = 2;
@@ -237,7 +237,7 @@ class RakLibInterface implements ServerInstance, AdvancedSourceInterface{
 
 			if($pk === null){
 				$pk = new EncapsulatedPacket();
-				$pk->buffer = $packet->buffer;
+				$pk->buffer =  chr("0x8e") . $packet->buffer;
 				if($packet->getChannel() !== 0){
 					$packet->reliability = 3;
 					$packet->orderChannel = $packet->getChannel();
@@ -260,12 +260,12 @@ class RakLibInterface implements ServerInstance, AdvancedSourceInterface{
 	}
 
 	private function getPacket($buffer){
-		$pid = ord($buffer{0});
+		$pid = ord($buffer{1});
 
 		if(($data = $this->network->getPacket($pid)) === null){
 			return null;
 		}
-		$data->setBuffer($buffer, 1);
+		$data->setBuffer($buffer, 2);
 
 		return $data;
 	}
