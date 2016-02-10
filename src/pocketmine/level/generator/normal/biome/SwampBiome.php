b16d796aa6790a7f7bca0531643258bbd2e17939
Muqsit Rayyan <hackhack05@gmail.com>
2016-02-10 18:55:03 +0300

    Flowery swamp baby..

--- src/pocketmine/level/generator/normal/biome/SwampBiome.php
+++ src/pocketmine/level/generator/normal/biome/SwampBiome.php
@@ -20,22 +20,25 @@
 */
 
 namespace pocketmine\level\generator\normal\biome;
-
+use pocketmine\level\generator\populator\Flower;
+use pocketmine\block\Block;
+use pocketmine\block\Flower as FlowerBlock;
 class SwampBiome extends GrassyBiome{
-
 	public function __construct(){
 		parent::__construct();
-
+		
+		$flower = new Flower();
+		$flower->setBaseAmount(8);
+		$flower->addType([Block::RED_FLOWER, FlowerBlock::TYPE_BLUE_ORCHID]);
+		
+		$this->addPopulator($flower);
 		$this->setElevation(62, 63);
-
 		$this->temperature = 0.8;
 		$this->rainfall = 0.9;
 	}
-
-	public function getName() : string{
+	public function getName(){
 		return "Swamp";
 	}
-
 	public function getColor(){
 		return 0x6a7039;
 	}
