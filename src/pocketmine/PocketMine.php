c7c1e8e0461d220f9d9a89a8b6e5124c6547d2c5
RandomAltThingy <goldowl37@gmail.com>
2016-02-10 21:28:26 +0000

    From ImagicalMine: Allow multiple 0.13 versions to join

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -75,8 +75,8 @@ namespace pocketmine {
 	const VERSION = "1.0dev";
 	const API_VERSION = "2.0.0";
 	const CODENAME = "Amazing PHP7!";
-	const MINECRAFT_VERSION = "v0.13.2 alpha";
-	const MINECRAFT_VERSION_NETWORK = "0.13.2";
+	const MINECRAFT_VERSION = "v0.13.x alpha";
+	const MINECRAFT_VERSION_NETWORK = "0.13.0";
 	const iTX_API_VERSION = '1.5.8';
 
 	/*
