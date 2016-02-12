ff25860cd22dbde2321ff2e57b323a681c0098d1
Muqsit Rayyan <hackhack05@gmail.com>
2016-02-12 07:56:09 +0300

    Mmhmm

--- src/pocketmine/level/generator/normal/biome/SwampBiome.php
+++ src/pocketmine/level/generator/normal/biome/SwampBiome.php
@@ -22,18 +22,19 @@
 namespace pocketmine\level\generator\normal\biome;
 namespace pocketmine\level\generator\populator\LilyPad;
 
-class SwampBiome extends OceanBiome{
+class SwampBiome extends GrassyBiome{
 
 	public function __construct(){
 		parent::__construct();
+
+		$lilypad = new LilyPad();
+		$lilypad->setBaseAmount(4);
 
 		$this->setElevation(62, 63);
 
 		$this->temperature = 0.8;
 		$this->rainfall = 0.9;
 
-		$lilypad = new LilyPad();
-		$lilypad->setBaseAmount(4);
 		$this->addPopulator($lilypad);
 	}
 
