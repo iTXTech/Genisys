178c6e16191e983263d8fda3e4b37c352dd5f66b
Hsinweic Chen <edwinyoo44@users.noreply.github.com>
2016-02-12 22:12:27 +0800

    Update PocketMine.php

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -63,8 +63,8 @@ namespace pocketmine {
 
 	const VERSION = "1.1dev-legacy";
 	const API_VERSION = "1.13.1";
-	const CODENAME = "support 0.13 and 0.14!";
-	const MINECRAFT_VERSION = "v0.13.2";
+	const CODENAME = "Hello, 2016 !";
+	const MINECRAFT_VERSION = "v0.13.x alpha";
 	const MINECRAFT_VERSION_NETWORK = "0.13.2";
 	const iTX_API_VERSION = '1.5.8';
 
