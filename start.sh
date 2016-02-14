c89f4050ef208156fe0ea12b64cae203fc051c49
Gao Yichuan <gaoyichuan000@gmail.com>
2016-02-14 22:37:40 +0800

    Fix start.sh (from PM)

--- start.sh
+++ start.sh
@@ -39,7 +39,7 @@ if [ "$PHP_BINARY" == "" ]; then
 	if [ -f ./bin/php7/bin/php ]; then
 		export PHPRC=""
 		PHP_BINARY="./bin/php7/bin/php"
-	elif [ type php 2>/dev/null ]; then
+	elif type php 2>/dev/null; then
 		PHP_BINARY=$(type -p php)
 	else
 		echo "Couldn't find a working PHP binary, please use the installer."
