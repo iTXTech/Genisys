00cc239def31598d58ed0671d151cffeed69dec0
Erkam Kahraman <erkamkahriman@gmail.com>
2016-02-21 12:57:21 +0100

    Update Sheep.php

--- src/pocketmine/entity/Sheep.php
+++ src/pocketmine/entity/Sheep.php
@@ -5,89 +5,42 @@
  *
  * @author PeratX
  */
-
 namespace pocketmine\entity;
 
-use pocketmine\block\Wool;
-use pocketmine\nbt\tag\ByteTag;
-use pocketmine\network\protocol\AddEntityPacket;
+use pocketmine\entity\Colorable;
+use pocketmine\item\Item;
 use pocketmine\Player;
-use pocketmine\item\Item as ItemItem;
-use pocketmine\level\format\FullChunk;
-use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\entity\Creature;
 
-class Sheep extends Animal implements Colorable{
-	const NETWORK_ID = 13;
+class Sheep extends WalkingAnimal implements Colorable{
+    const NETWORK_ID = 13;
 
-	const DATA_COLOR_INFO = 16;
+    public $width = 1.45;
+    public $height = 1.12;
 
-	public $width = 0.625;
-	public $length = 1.4375;
-	public $height = 1.8;
-	
-	public function getName() : string{
-		return "Sheep";
-	}
+    public function getName(){
+        return "Sheep";
+    }
 
-	public function __construct(FullChunk $chunk, CompoundTag $nbt){
-		if(!isset($nbt->Color)){
-			$nbt->Color = new ByteTag("Color", self::getRandomColor());
-		}
-		parent::__construct($chunk, $nbt);
+    public function initEntity(){
+        parent::initEntity();
 
-		$this->setDataProperty(self::DATA_COLOR_INFO, self::DATA_TYPE_BYTE, $this->getColor());
-	}
+        $this->setMaxHealth(8);
+    }
 
-	public static function getRandomColor() : int{
-		$rand = "";
-		$rand .= str_repeat(Wool::WHITE . " ", 20);
-		$rand .= str_repeat(Wool::ORANGE . " ", 5);
-		$rand .= str_repeat(Wool::MAGENTA . " ", 5);
-		$rand .= str_repeat(Wool::LIGHT_BLUE . " ", 5);
-		$rand .= str_repeat(Wool::YELLOW . " ", 5);
-		$rand .= str_repeat(Wool::GRAY . " ", 10);
-		$rand .= str_repeat(Wool::LIGHT_GRAY . " ", 10);
-		$rand .= str_repeat(Wool::CYAN . " ", 5);
-		$rand .= str_repeat(Wool::PURPLE . " ", 5);
-		$rand .= str_repeat(Wool::BLUE . " ", 5);
-		$rand .= str_repeat(Wool::BROWN . " ", 5);
-		$rand .= str_repeat(Wool::GREEN . " ", 5);
-		$rand .= str_repeat(Wool::RED . " ", 5);
-		$rand .= str_repeat(Wool::BLACK . " ", 10);
-		$arr = explode(" ", $rand);
-		return $arr[mt_rand(0, count($arr) - 1)];
-	}
+    public function targetOption(Creature $creature, float $distance) : bool{
+        if($creature instanceof Player){
+            return $creature->spawned && $creature->isAlive() && !$creature->closed && $creature->getInventory()->getItemInHand()->getId() == Item::SEEDS && $distance <= 49;
+        }
+        return false;
+    }
 
-	public function getColor() : int{
-		return (int) $this->namedtag["Color"];
-	}
+    public function getDrops(){
+        if($this->lastDamageCause instanceof EntityDamageByEntityEvent){
+            return [Item::get(Item::WOOL, mt_rand(0, 15), 1)];
+        }
+        return [];
+    }
 
-	public function setColor(int $color){
-		$this->namedtag->Color = new ByteTag("Color", $color);
-	}
-	
-	public function spawnTo(Player $player){
-		$pk = new AddEntityPacket();
-		$pk->eid = $this->getId();
-		$pk->type = Sheep::NETWORK_ID;
-		$pk->x = $this->x;
-		$pk->y = $this->y;
-		$pk->z = $this->z;
-		$pk->speedX = $this->motionX;
-		$pk->speedY = $this->motionY;
-		$pk->speedZ = $this->motionZ;
-		$pk->yaw = $this->yaw;
-		$pk->pitch = $this->pitch;
-		$pk->metadata = $this->dataProperties;
-		$player->dataPacket($pk);
-
-		parent::spawnTo($player);
-	}
-	
-	public function getDrops(){
-		$drops = [
-			ItemItem::get(ItemItem::WOOL, $this->getColor(), 1)
-		];
-		return $drops;
-	}
 }
