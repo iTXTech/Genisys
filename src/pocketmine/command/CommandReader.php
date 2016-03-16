492895e6f593557df2543efbadd7a1a64aefe88c
Matthew <matthew.hilbrink@outlook.com>
2016-03-16 20:21:46 +0100

    Revert https://github.com/iTXTech/Genisys/commit/3bcdb2a31c450512a2284af9b6918b5b82b5fc44
    Revert https://github.com/iTXTech/Genisys/commit/3bcdb2a31c450512a2284af9b6918b5b82b5fc44
    
    Fixes issue #616

--- src/pocketmine/command/CommandReader.php
+++ src/pocketmine/command/CommandReader.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  *
  *  ____            _        _   __  __ _                  __  __ ____
@@ -18,47 +17,36 @@
  *
  *
 */
-
 namespace pocketmine\command;
-
 use pocketmine\Thread;
-
 class CommandReader extends Thread{
 	private $readline;
 	/** @var \Threaded */
 	protected $buffer;
 	private $shutdown = false;
-
 	public function __construct(){
 		$this->buffer = new \Threaded;
 		$this->start();
 		//$this->start(PTHREADS_INHERIT_NONE);//May cause segfault
 	}
-
 	public function shutdown(){
 		$this->shutdown = true;
 	}
-
 	private function readLine(){
 		if(!$this->readline){
-			/*global $stdin;
-
+			global $stdin;
 			if(!is_resource($stdin)){
 				return "";
 			}
-
-			return trim(fgets($stdin));*/
-			return trim(fgets(fopen("php://stdin", "r")));
+			return trim(fgets($stdin));
 		}else{
 			$line = trim(readline("> "));
 			if($line != ""){
 				readline_add_history($line);
 			}
-
 			return $line;
 		}
 	}
-
 	/**
 	 * Reads a line from console, if available. Returns null if not available
 	 *
@@ -68,37 +56,31 @@ class CommandReader extends Thread{
 		if($this->buffer->count() !== 0){
 			return $this->buffer->shift();
 		}
-
 		return null;
 	}
-
 	public function quit(){
 		$this->shutdown();
 	}
-
 	public function run(){
 		$opts = getopt("", ["disable-readline"]);
 		if(extension_loaded("readline") and !isset($opts["disable-readline"])){
 			$this->readline = true;
 		}else{
-		/*	global $stdin;
+			global $stdin;
 			$stdin = fopen("php://stdin", "r");
-			stream_set_blocking($stdin, 0);*/
+			stream_set_blocking($stdin, 0);
 			$this->readline = false;
 		}
-
 		$lastLine = microtime(true);
-		while(!$this->shutdown){
+		while(!$this->shutdown){;
 			if(($line = $this->readLine()) !== ""){
 				$this->buffer[] = preg_replace("#\\x1b\\x5b([^\\x1b]*\\x7e|[\\x40-\\x50])#", "", $line);
 			}elseif(!$this->shutdown and (microtime(true) - $lastLine) <= 0.1){ //Non blocking! Sleep to save CPU
 				usleep(40000);
 			}
-
 			$lastLine = microtime(true);
 		}
 	}
-
 	public function getThreadName(){
 		return "Console";
 	}
