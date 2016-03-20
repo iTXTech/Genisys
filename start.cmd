1bf908075acfe757758bcab2048b7fd2679ff1a7
PeratX <1215714524@qq.com>
2016-03-20 16:09:31 +0800

    Improved start.cmd

--- start.cmd
+++ start.cmd
@@ -1,5 +1,5 @@
 @echo off
-TITLE PocketMine-iTX server software for Minecraft: Pocket Edition
+TITLE Genisys server software for Minecraft: Pocket Edition
 cd /d %~dp0
 
 if exist bin\php\php.exe (
@@ -9,32 +9,28 @@ if exist bin\php\php.exe (
 	set PHP_BINARY=php
 )
 
-if exist Genisys.phar (
-	set POCKETMINE_FILE=Genisys.phar
+if exist Genisys*.phar (
+	set POCKETMINE_FILE=Genisys*.phar
 ) else (
-   if exist PocketMine-MP.phar (
-	set POCKETMINE_FILE=PocketMine-MP.phar
+    if exist PocketMine-MP.phar (
+        set POCKETMINE_FILE=PocketMine-MP.phar
 	) else (
-	 if exist PocketMine-Soft.phar (
-	   set POCKETMINE_FILE=PocketMine-Soft.phar
-     	    ) else (
-    	      if exist src\pocketmine\PocketMine.php (
-	       set POCKETMINE_FILE=src\pocketmine\PocketMine.php
-                 ) else (
-		    if exist Genisys*.phar (
-			set POCKETMINE_FILE=Genisys*.phar
-			   ) else (
-		echo "[ERROR] Couldn't find a valid Genisys installation."
-		pause
-		exit 8
-	)
-	)
-	)
+	    if exist src\pocketmine\PocketMine.php (
+	        set POCKETMINE_FILE=src\pocketmine\PocketMine.php
+        ) else (
+            if exist Genisys.phar (
+                set POCKETMINE_FILE=Genisys.phar
+            ) else (
+		        echo "[ERROR] Couldn't find a valid Genisys installation."
+		        pause
+		        exit 8
+		    )
+	    )
 	)
 )
 
-	if exist bin\mintty.exe (
-		start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="DejaVu Sans Mono" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "PocketMine-iTX" -i bin/pocketmine.ico -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
-	) else (
-		%PHP_BINARY% -c bin\php %POCKETMINE_FILE% %*
-	)
+if exist bin\mintty.exe (
+	start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="DejaVu Sans Mono" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "PocketMine-iTX" -i bin/pocketmine.ico -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
+) else (
+	%PHP_BINARY% -c bin\php %POCKETMINE_FILE% %*
+)
