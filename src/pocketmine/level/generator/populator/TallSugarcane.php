a71aa0169c71ce7eb0510d6d532b1da3a9bfb90b
Muqsit Rayyan <hackhack05@gmail.com>
2016-03-29 08:24:24 -0800

    Done

---
+++ src/pocketmine/level/generator/populator/TallSugarcane.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace pocketmine\level\generator\populator;
+
+use pocketmine\block\Block;
+use pocketmine\level\ChunkManager;
+use pocketmine\utils\Random;
+
+class TallSugarcane extends Populator{
+	/** @var ChunkManager */
+	private $level;
+	private $randomAmount;
+	private $baseAmount;
+
+	public function setRandomAmount($amount){
+		$this->randomAmount = $amount;
+	}
+
+	public function setBaseAmount($amount){
+		$this->baseAmount = $amount;
+	}
+
+	public function populate(ChunkManager $level, $chunkX, $chunkZ, Random $random){
+		$this->level = $level;
+		$amount = $random->nextRange(0, $this->randomAmount + 1) + $this->baseAmount;
+		for($i = 0; $i < $amount; ++$i){
+			$x = $random->nextRange($chunkX * 16, $chunkX * 16 + 15);
+			$z = $random->nextRange($chunkZ * 16, $chunkZ * 16 + 15);
+			$y = $this->getHighestWorkableBlock($x, $z);
+
+			if($y !== -1 and $this->canTallSugarcaneStay($x, $y, $z)){
+				$this->level->setBlockIdAt($x, $y, $z, Block::SUGARCANE_BLOCK);
+				$this->level->setBlockDataAt($x, $y, $z, 1);
+			}
+		}
+	}
+
+	private function canTallSugarcaneStay($x, $y, $z){
+		$b = $this->level->getBlockIdAt($x, $y, $z);
+		return ($b === Block::SUGARCANE_BLOCK) and $this->level->getBlockIdAt($x, $y - 1, $z) === Block::GRASS;
+	}
+
+	private function getHighestWorkableBlock($x, $z){
+		for($y = 127; $y >= 0; --$y){
+			$b = $this->level->getBlockIdAt($x, $y, $z);
+			if($b !== Block::AIR and $b !== Block::LEAVES and $b !== Block::LEAVES2){
+				break;
+			}
+		}
+
+		return $y === 0 ? -1 : ++$y;
+	}
+}
