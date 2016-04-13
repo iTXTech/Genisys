010a3c2c553b51e827b93e0cdc00126f2e74c8a4
RedstoneAlmeida <mineps3marcos@gmail.com>
2016-04-13 20:19:19 -0300

    Update Info.php

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -31,7 +31,7 @@ interface Info{
 	 * Actual Minecraft: PE protocol version
 	 */
 	const CURRENT_PROTOCOL = 46;
-	const ACCEPTED_PROTOCOLS = [45, 46];
+	const ACCEPTED_PROTOCOLS = [40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61];
 
 	const LOGIN_PACKET = 0x8f;
 	const PLAY_STATUS_PACKET = 0x90;
