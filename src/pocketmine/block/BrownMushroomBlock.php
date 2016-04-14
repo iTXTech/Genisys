26ca2d6d3ab5b29c36d52139d023c099dcfa4af6
dog194 <514896768@qq.com>
2016-04-14 12:46:42 +0800

    delete wrong/useless code

--- src/pocketmine/block/BrownMushroomBlock.php
+++ src/pocketmine/block/BrownMushroomBlock.php
@@ -4,9 +4,7 @@
 namespace pocketmine\block;
 
 use pocketmine\item\Item;
-use pocketmine\item\Tool;
 use pocketmine\item\enchantment\enchantment;
-use pocketmine\Player;
 
 class BrownMushroomBlock extends Solid{
 	
@@ -14,20 +12,12 @@ class BrownMushroomBlock extends Solid{
 
 	protected $id = self::BROWN_MUSHROOM_BLOCK;
 
-	public function __construct($meta = 15){
+	public function __construct($meta = 14){
 		$this->meta = $meta;
 	}
 
 	public function canBeActivated() : bool {
 		return true;
-	}
-
-	public function getHardness() {
-		return 0.5;
-	}
-
-	public function getToolType(){
-		return Tool::TYPE_AXE;
 	}
 
 	public function getName() : string{
