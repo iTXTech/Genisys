b73b4d9018d6c4bdcd5906313c003457714cf383
dog194 <514896768@qq.com>
2016-04-14 23:49:16 +0800

    Update EmeraldOre.php

--- src/pocketmine/block/EmeraldOre.php
+++ src/pocketmine/block/EmeraldOre.php
@@ -52,8 +52,12 @@ class EmeraldOre extends Solid{
 					[Item::EMERALD_ORE, 0, 1],
 				];
 			}else{
+				$fortunel = $item->getEnchantmentLevel(Enchantment::TYPE_MINING_FORTUNE);
+				$fortunel = $fortunel > 3 ? 3 : $fortunel;
+				$times = [1,1,2,3,4];
+				$time = $times[mt_rand(0, $fortunel + 1)];
 				return [
-					[Item::EMERALD, 0, 1],
+					[Item::EMERALD, 0, $time],
 				];
 			}
 		}else{
