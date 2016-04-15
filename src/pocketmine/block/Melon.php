4508eac4e2694d090c1da305a931b73880da157c
dog194 <514896768@qq.com>
2016-04-15 23:07:42 +0800

    Update Melon.php

--- src/pocketmine/block/Melon.php
+++ src/pocketmine/block/Melon.php
@@ -51,10 +51,11 @@ class Melon extends Transparent{
 				[Item::MELON_BLOCK, 0, 1],
 			];
 		}else{
+			$fortunel = $item->getEnchantmentLevel(Enchantment::TYPE_MINING_FORTUNE);
+			$fortunel = $fortunel > 2 ? 2 : $fortunel; //Note: for Melon level 2 is the same 3 So highest is 2
 			return [
-				[Item::MELON_SLICE, 0, mt_rand(3, 7)],
+				[Item::MELON_SLICE, 0, mt_rand(3, 7 + $fortunel)],
 			];
 		}
-		
 	}
 }
