061dc14fbb7a26f36c3b59ff1bbabdf0d1c20e02
dog194 <514896768@qq.com>
2016-04-15 23:04:34 +0800

    Update Gravel.php

--- src/pocketmine/block/Gravel.php
+++ src/pocketmine/block/Gravel.php
@@ -46,15 +46,20 @@ class Gravel extends Fallable{
 	}
 
 	public function getDrops(Item $item) : array {
-		if(mt_rand(1, 10) === 1 and $item->getEnchantmentLevel(Enchantment::TYPE_MINING_SILK_TOUCH) === 0){//使用精准采集附魔 不掉落燧石
-			return [
-				[Item::FLINT, 0, 1],
-			];
+		$drops = [];
+		if($item->getEnchantmentLevel(Enchantment::TYPE_MINING_SILK_TOUCH) > 0){//使用精准采集附魔 不掉落燧石
+			$drops[] = [Item::GRAVEL, 0, 1];
+			return $drops;
 		}
-
-		return [
-			[Item::GRAVEL, 0, 1],
-		];
+		$fortunel = $item->getEnchantmentLevel(Enchantment::TYPE_MINING_FORTUNE);
+		$fortunel = $fortunel > 3 ? 3 : $fortunel;
+		$rates = [10,7,4,1];
+		if(mt_rand(1, $rates[$fortunel]) === 1){//10% 14% 25% 100%
+			$drops[] = [Item::FLINT, 0, 1];
+		}
+		if(mt_rand(1, 10) !== 1){//90%
+			$drops[] = [Item::GRAVEL, 0, 1];
+		}
+		return $drops;
 	}
-
 }
