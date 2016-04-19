41321fbb9244a12cfb91276b8dc56a55c2b048ac
PeratX <1215714524@qq.com>
2016-04-19 14:39:48 +0800

    Format

--- src/pocketmine/block/Tripwire.php
+++ src/pocketmine/block/Tripwire.php
@@ -23,29 +23,28 @@ namespace pocketmine\block;
 
 use pocketmine\item\Tool;
 
-class Tripwire extends Solid {
+class Tripwire extends Solid{
 
-    protected $id = self::TRIPWIRE;
+	protected $id = self::TRIPWIRE;
 
-    public function __construct($meta = 0){
-        $this->meta = $meta;
-    }
+	public function __construct($meta = 0){
+		$this->meta = $meta;
+	}
 
-    public function getName() : string {
-        return "Tripwire";
-    }
+	public function getName() : string{
+		return "Tripwire";
+	}
 
-    public function getToolType()
-    {
-        return Tool::TYPE_SHEARS;
-    }
+	public function getToolType(){
+		return Tool::TYPE_SHEARS;
+	}
 
-    public function getHardness() {
-        return 0;
-    }
+	public function getHardness(){
+		return 0;
+	}
 
-    public function getResistance(){
-        return 0;
-    }
+	public function getResistance(){
+		return 0;
+	}
 
 }
