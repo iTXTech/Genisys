03d2aa7c65c3446bad4fe6135d21c8c626021968
Tatsuyuki Ishi <ishitatsuyuki@gmail.com>
2016-04-23 18:47:38 +0900

    CI: Fix quote

--- tests/ci.sh
+++ tests/ci.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo -e "version\nms\nstop\n\n" | php src/pocketmine/PocketMine.php --no-wizard --disable-readline"
+echo -e "version\nms\nstop\n\n" | php src/pocketmine/PocketMine.php --no-wizard --disable-readline
 exit $?
