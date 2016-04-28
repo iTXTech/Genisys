91977516b0df5f7dd8d196ade8e5f63aa1144a43
PolarKing <CLiu13@users.noreply.github.com>
2016-04-27 23:38:09 -0400

    Update Player.php

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -1422,7 +1422,7 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 		$pk = new AdventureSettingsPacket();
 		$pk->flags = $flags;
 		$pk->userPermission = 2;
- +		$pk->globalPermission = 2;
+		$pk->globalPermission = 2;
 		$this->dataPacket($pk);
 	}
 
