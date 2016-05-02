5eda03180c8ef38ba99151f2b96a3110d1b41dda
Tatsuyuki Ishi <ishitatsuyuki@gmail.com>
2016-05-02 12:03:26 +0000

    Update StoneButton.php

--- src/pocketmine/block/StoneButton.php
+++ src/pocketmine/block/StoneButton.php
@@ -22,7 +22,7 @@
 namespace pocketmine\block;
 
 use pocketmine\item\Item;
-use pocketmine\player\Player;
+use pocketmine\Player;
 
 class StoneButton extends WoodenButton{
 	protected $id = self::STONE_BUTTON;
