759ceb8e741287039f21fd3225f0f2084119f6c4
dog194 <514896768@qq.com>
2016-05-04 19:55:47 +0800

    Add Drops and enchantment for SnowBlock

--- src/pocketmine/block/Snow.php
+++ src/pocketmine/block/Snow.php
@@ -21,27 +21,38 @@
 
 namespace pocketmine\block;
 
-
-use pocketmine\item\Tool;
+use pocketmine\item\Item;
+use pocketmine\item\enchantment\enchantment;
 
 class Snow extends Solid{
 
 	protected $id = self::SNOW_BLOCK;
 
-	public function __construct(){
-
+	public function __construct($meta = 0){
+		$this->meta = $meta;
 	}
 
 	public function getHardness() {
 		return 0.2;
 	}
 
-	public function getToolType(){
-		return Tool::TYPE_SHOVEL;
-	}
-
 	public function getName() : string{
 		return "Snow Block";
 	}
-
+	
+	public function getDrops(Item $item) : array {
+		if($item->isShovel() !== false){
+			if($item->getEnchantmentLevel(Enchantment::TYPE_MINING_SILK_TOUCH) > 0){
+				return [
+					[Item::SNOW_BLOCK, 0, 1],
+				];
+			}else{
+				return [
+					[Item::SNOWBALL, 0, 4],
+				];
+			}
+		}else{
+			return [];
+		}
+	}
 }
