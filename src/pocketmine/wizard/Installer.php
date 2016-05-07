9752aa34b8cd4638e1ee2825d13e67c844dbe1d7
XFuryMCPE <xfurymcpe@gmail.com>
2016-05-06 23:16:06 -0400

    Added more options to the installer
    More options when starting a new server.

--- src/pocketmine/wizard/Installer.php
+++ src/pocketmine/wizard/Installer.php
@@ -34,6 +34,8 @@ class Installer{
 	const DEFAULT_MEMORY = 512;
 	const DEFAULT_PLAYERS = 20;
 	const DEFAULT_GAMEMODE = 0;
+	const DEFAULT_LEVEL_NAME = "world";
+	const DEFAULT_LEVEL_TYPE = "default";
 
 	private $defaultLang;
 
@@ -112,6 +114,7 @@ LICENSE;
 		$config = new Config(\pocketmine\DATA . "server.properties", Config::PROPERTIES);
 		echo "[?] " . $this->lang->name_your_server . " (" . self::DEFAULT_NAME . "): ";
 		$config->set("server-name", $this->getInput(self::DEFAULT_NAME));
+		$config->set("motd", $this->getInput(self::DEFAULT_NAME));
 		echo "[*] " . $this->lang->port_warning . "\n";
 		do{
 			echo "[?] " . $this->lang->server_port . " (" . self::DEFAULT_PORT . "): ";
@@ -121,6 +124,19 @@ LICENSE;
 			}
 		}while($port <= 0 or $port > 65535);
 		$config->set("server-port", $port);
+		
+		echo "[?] " . $this->lang->level_name . " (" . self::DEFAULT_LEVEL_NAME . "): ";
+		$config->set("level-name", $this->getInput(self::DEFAULT_LEVEL_NAME));
+		
+		do{
+			echo "[?] " . $this->lang->level_type . " (" . self::DEFAULT_LEVEL_TYPE . "): ";
+			$type = (string) $this->getInput(self::DEFAULT_LEVEL_TYPE);
+			if($type != "flat" or $type != "default"){
+				echo "[!] " . $this->lang->invalid_level_type . "\n";
+			}
+		}while($type == "flat" or $type == "default");
+		$config->set("level-type", $type);
+		
 		/*echo "[*] " . $this->lang->ram_warning . "\n";
 		echo "[?] " . $this->lang->server_ram . " (" . self::DEFAULT_MEMORY . "): ";
 		$config->set("memory-limit", ((int) $this->getInput(self::DEFAULT_MEMORY)) . "M");*/
@@ -138,6 +154,13 @@ LICENSE;
 			$config->set("spawn-protection", -1);
 		}else{
 			$config->set("spawn-protection", 16);
+		}
+		
+		echo "[?] " . $this->lang->announce_player_achievements . " (y/N): ";
+		if(strtolower($this->getInput("n")) === "y"){
+			$config->set("announce-player-achievements", "on");
+		}else{
+			$config->set("announce-player-achievements", "off");
 		}
 		$config->save();
 	}
