6af1f14a9c97b9654f03f733fc046873605054df
jojoe77777 <jojoefivesevens@gmail.com>
2016-05-19 19:33:30 +1000

    Bumped to 0.14.3

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -30,8 +30,8 @@ interface Info{
 	/**
 	 * Actual Minecraft: PE protocol version
 	 */
-	const CURRENT_PROTOCOL = 60;
-	const ACCEPTED_PROTOCOLS = [45, 46, 60];
+	const CURRENT_PROTOCOL = 70;
+	const ACCEPTED_PROTOCOLS = [45, 46, 60, 70];
 
 	const LOGIN_PACKET = 0x8f;
 	const PLAY_STATUS_PACKET = 0x90;
