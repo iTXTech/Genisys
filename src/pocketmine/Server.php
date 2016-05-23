558dc64bd966aa53999446f41cb0a84df072adf7
Victor T <gamecraftervt@gmail.com>
2016-05-22 17:18:23 -0700

    Update Server.php

--- src/pocketmine/Server.php
+++ src/pocketmine/Server.php
@@ -1777,7 +1777,7 @@ class Server{
 		   §5PocketMine-iTX §3Genisys §fis a fork of PocketMine-MP, made by §5iTX Technologies LLC§f.
 		   §fVersion: §6" . $this->getPocketMineVersion() . "
 		   §fTarget client Version: §d" . \pocketmine\MINECRAFT_VERSION . "
-		   §fLatest source codes are available on https://github.com/iTXTech/Genisys
+		   §fLatest source code is available at https://github.com/iTXTech/Genisys
 		   §fDonate link: http://pl.zxda.net/plugins/203.html
 		   §f如果你在免费使用本核心，希望你可以进入上面的链接捐赠给我们，这会成为我们前进的动力。
 		\n";
