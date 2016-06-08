041b587aaf85e208dac5055d5e3d20c695ac1b3b
thebigsmileXD <thebigsmilexd@gmail.com>
2016-06-08 13:18:11 +0200

    Create sign tile like it should be.

--- src/pocketmine/block/SignPost.php
+++ src/pocketmine/block/SignPost.php
@@ -25,7 +25,11 @@ use pocketmine\item\Item;
 use pocketmine\item\Tool;
 use pocketmine\level\Level;
 use pocketmine\math\Vector3;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\IntTag;
+use pocketmine\nbt\tag\StringTag;
 use pocketmine\Player;
+use pocketmine\tile\Tile;
 
 class SignPost extends Transparent{
 
@@ -54,23 +58,38 @@ class SignPost extends Transparent{
 
 	public function place(Item $item, Block $block, Block $target, $face, $fx, $fy, $fz, Player $player = null){
 		if($face !== 0){
-			$faces = [
-				2 => 2,
-				3 => 3,
-				4 => 4,
-				5 => 5,
-			];
-			if(!isset($faces[$face])){
-				$this->meta = floor((($player->yaw + 180) * 16 / 360) + 0.5) & 0x0F;
-				$this->getLevel()->setBlock($block, Block::get(Item::SIGN_POST, $this->meta), true);
+			$nbt = new CompoundTag("", [
+				"id" => new StringTag("id", Tile::SIGN),
+				"x" => new IntTag("x", $block->x),
+				"y" => new IntTag("y", $block->y),
+				"z" => new IntTag("z", $block->z),
+				"Text1" => new StringTag("Text1", ""),
+				"Text2" => new StringTag("Text2", ""),
+				"Text3" => new StringTag("Text3", ""),
+				"Text4" => new StringTag("Text4", "")
+			]);
 
-				return true;
-			}else{
-				$this->meta = $faces[$face];
-				$this->getLevel()->setBlock($block, Block::get(Item::WALL_SIGN, $this->meta), true);
-
-				return true;
+			if($player !== null){
+				$nbt->Creator = new StringTag("Creator", $player->getRawUniqueId());
 			}
+
+			if($item->hasCustomBlockData()){
+				foreach($item->getCustomBlockData() as $key => $v){
+					$nbt->{$key} = $v;
+				}
+			}
+
+			if($face === 1){
+				$this->meta = floor((($player->yaw + 180) * 16 / 360) + 0.5) & 0x0f;
+				$this->getLevel()->setBlock($block, $this, true);
+			}else{
+				$this->meta = $face;
+				$this->getLevel()->setBlock($block, new WallSign($this->meta), true);
+			}
+
+			Tile::createTile(Tile::SIGN, $this->getLevel()->getChunk($block->x >> 4, $block->z >> 4), $nbt);
+
+			return true;
 		}
 
 		return false;
