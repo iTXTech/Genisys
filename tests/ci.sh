52dcf47594599e509b7c6a8f445b25b1caa39250
Tatsuyuki Ishi <ishitatsuyuki@gmail.com>
2016-06-11 18:35:13 +0900

    CI: even more silence

--- tests/ci.sh
+++ tests/ci.sh
@@ -6,7 +6,7 @@ for file in **/*.php; do
     [ $? -ne 0 ] && echo -n "$OUTPUT" && exit 1
 done
 echo Lint done successfully.
-echo -e "version\nms\nstop\n" | php src/pocketmine/PocketMine.php --no-wizard | grep -v "\[Genisys\] Adding .*\.php"
+echo -e "version\nms\nstop\n" | php src/pocketmine/PocketMine.php --no-wizard | grep -v "\[Genisys\] Adding "
 if ls plugins/Genisys/Genisys*.phar >/dev/null 2>&1; then
     echo Server packaged successfully.
 else
