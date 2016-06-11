8c899226c41aca3b7e39115bce6850f789e2c744
Tatsuyuki Ishi <ishitatsuyuki@gmail.com>
2016-06-11 18:31:56 +0900

    CI: more silence

--- tests/ci.sh
+++ tests/ci.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+echo Running lint...
 shopt -s globstar
 for file in **/*.php; do
     OUTPUT=`php -l "$file"`
     [ $? -ne 0 ] && echo -n "$OUTPUT" && exit 1
 done
-echo -e "version\nms\nstop\n" | php src/pocketmine/PocketMine.php --no-wizard
+echo Lint done successfully.
+echo -e "version\nms\nstop\n" | php src/pocketmine/PocketMine.php --no-wizard | grep -v "\[Genisys\] Adding .*\.php"
 if ls plugins/Genisys/Genisys*.phar >/dev/null 2>&1; then
-    echo "Server packaged successfully."
+    echo Server packaged successfully.
 else
-    echo "No phar created!"
+    echo No phar created!
     exit 1
 fi
