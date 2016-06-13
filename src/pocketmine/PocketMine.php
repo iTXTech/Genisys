bcc6f2fa59d30e0f23a55707a7e2c336ec38e4d3
Professor Taki <Taki21@users.noreply.github.com>
2016-06-13 15:52:43 -0400

    0.15.0 Has officially come out

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -75,8 +75,8 @@ namespace pocketmine {
 	const VERSION = "1.1dev";
 	const API_VERSION = "2.0.0";
 	const CODENAME = "Ikaros";
-	const MINECRAFT_VERSION = "v0.14.x alpha";
-	const MINECRAFT_VERSION_NETWORK = "0.14.3";
+	const MINECRAFT_VERSION = "v0.15.0 alpha";
+	const MINECRAFT_VERSION_NETWORK = "0.15.0";
 	const GENISYS_API_VERSION = '1.7.3';
 
 	/*
