dd27242be91e2f83c8d7587b217d93bdaf2b0749
XFuryMCPE <xfurymcpe@gmail.com>
2016-06-16 15:45:01 -0400

    Fix blocks not updating

--- src/pocketmine/network/protocol/UpdateBlockPacket.php
+++ src/pocketmine/network/protocol/UpdateBlockPacket.php
@@ -44,7 +44,6 @@ class UpdateBlockPacket extends DataPacket{
 
 	public function encode(){
 		$this->reset();
-		$this->putInt(count($this->records));
 		foreach($this->records as $r){
 			$this->putInt($r[0]);
 			$this->putInt($r[1]);
