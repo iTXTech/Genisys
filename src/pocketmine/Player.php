e707f65d45dfd99d0057b1000ddccbaf11dfdbd5
XFuryMCPE <xfurymcpe@gmail.com>
2016-06-16 17:39:36 -0400

    remove lead fix to stop conflict with other pull

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -2454,13 +2454,6 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 			$this->dataPacket($pk);
 		}
 		
-		$pk = new SetEntityDataPacket();
-		$pk->eid = 0;
-		$pk->metadata = [
-			self::DATA_LEAD_HOLDER => [self::DATA_TYPE_LONG, -1]
-		];
-		$this->dataPacket($pk);
-		
 		$this->forceMovement = $this->teleportPosition = $this->getPosition();
 	}
 
