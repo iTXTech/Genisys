97da0933c25410f8a5af69134d3212a9e0bfa57a
PeratX <1215714524@qq.com>
2016-06-23 15:00:52 +0800

    Fixed #1245

--- src/pocketmine/level/particle/FloatingTextParticle.php
+++ src/pocketmine/level/particle/FloatingTextParticle.php
@@ -23,9 +23,12 @@ namespace pocketmine\level\particle;
 
 use pocketmine\entity\Entity;
 use pocketmine\entity\Item as ItemEntity;
+use pocketmine\item\Item;
 use pocketmine\math\Vector3;
 use pocketmine\network\protocol\AddEntityPacket;
+use pocketmine\network\protocol\AddPlayerPacket;
 use pocketmine\network\protocol\RemoveEntityPacket;
+use pocketmine\utils\UUID;
 
 class FloatingTextParticle extends Particle{
 	//TODO: HACK!
@@ -76,19 +79,18 @@ class FloatingTextParticle extends Particle{
 
 		if(!$this->invisible){
 			
-			$pk = new AddEntityPacket();
+			$pk = new AddPlayerPacket();
 			$pk->eid = $this->entityId;
-			$pk->type = ItemEntity::NETWORK_ID;
+			$pk->uuid = UUID::fromRandom();
 			$pk->x = $this->x;
-			$pk->y = $this->y - 0.75;
+			$pk->y = $this->y - 1.62;
 			$pk->z = $this->z;
 			$pk->speedX = 0;
 			$pk->speedY = 0;
 			$pk->speedZ = 0;
 			$pk->yaw = 0;
 			$pk->pitch = 0;
-			$pk->item = 0;
-			$pk->meta = 0;
+			$pk->item = Item::get(0);
 			$pk->metadata = [
 				Entity::DATA_FLAGS => [Entity::DATA_TYPE_BYTE, 1 << Entity::DATA_FLAG_INVISIBLE],
 				Entity::DATA_NAMETAG => [Entity::DATA_TYPE_STRING, $this->title . ($this->text !== "" ? "\n" . $this->text : "")],
