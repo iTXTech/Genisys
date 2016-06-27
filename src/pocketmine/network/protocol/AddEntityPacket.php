0079861d3ac0dc72df97c06f4a84928dc0e8d4b4
jojoe77777 <j@melban.com>
2016-06-27 15:51:25 +1000

    Fixes entities with metadata causing crashes (#1275)
    * Fixes entities with metadata causing crashes

--- src/pocketmine/network/protocol/AddEntityPacket.php
+++ src/pocketmine/network/protocol/AddEntityPacket.php
@@ -41,6 +41,7 @@ class AddEntityPacket extends DataPacket{
 	public $speedZ;
 	public $yaw;
 	public $pitch;
+	public $modifiers;
 	public $metadata = [];
 	public $links = [];
 
@@ -58,24 +59,17 @@ class AddEntityPacket extends DataPacket{
 		$this->putFloat($this->speedX);
 		$this->putFloat($this->speedY);
 		$this->putFloat($this->speedZ);
-		//$this->putFloat($this->yaw);
-		//$this->putFloat($this->pitch);
-		//$meta = Binary::writeMetadata($this->metadata);
-		//$this->put($meta);
 		$this->putFloat($this->yaw * 0.71111);
 		$this->putFloat($this->pitch * 0.71111);
-		$this->putShort(0);
-		if(!empty($this->metadata)) {
-			$meta = Binary::writeMetadata($this->metadata);
-			$this->put($meta);
-		}
-		$this->putShort(0);
-		/*$this->putShort(count($this->links));
+		$this->putInt($this->modifiers);
+		$meta = Binary::writeMetadata($this->metadata);
+		$this->put($meta);
+		$this->putShort(count($this->links));
 		foreach($this->links as $link){
 			$this->putLong($link[0]);
 			$this->putLong($link[1]);
 			$this->putByte($link[2]);
-		}*/
+		}
 	}
 
 }
