bec5a26fe9c98f7b033d4566fa367e7c9c5733d0
dog194 <514896768@qq.com>
2016-07-02 14:06:26 +0800

    FIx #1320
    FIx $entity->getInventory()->getArmorContents() on null

--- src/pocketmine/event/entity/EntityDamageEvent.php
+++ src/pocketmine/event/entity/EntityDamageEvent.php
@@ -24,6 +24,7 @@ namespace pocketmine\event\entity;
 use pocketmine\entity\Effect;
 use pocketmine\entity\Entity;
 use pocketmine\event\Cancellable;
+use pocketmine\inventory\PlayerInventory;
 use pocketmine\Player;
 use pocketmine\item\Item;
 use pocketmine\item\enchantment\enchantment;
@@ -106,7 +107,7 @@ class EntityDamageEvent extends EntityEvent implements Cancellable{
 		}
 
 		//TODO: add zombie
-		if($entity instanceof Player){
+		if($entity instanceof Player and $entity->getInventory() instanceof PlayerInventory){
 			switch($cause){
 				case self::CAUSE_CONTACT:
 				case self::CAUSE_ENTITY_ATTACK:
