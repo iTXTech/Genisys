93f32ac1c87e8dded0e5d67a598dc24e743e6f52
PeratX <1215714524@qq.com>
2016-07-04 01:57:06 +0800

    Added WhiteSmokeParticle

---
+++ src/pocketmine/level/particle/WhiteSmokeParticle.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ *
+ *  _____   _____   __   _   _   _____  __    __  _____
+ * /  ___| | ____| |  \ | | | | /  ___/ \ \  / / /  ___/
+ * | |     | |__   |   \| | | | | |___   \ \/ /  | |___
+ * | |  _  |  __|  | |\   | | | \___  \   \  /   \___  \
+ * | |_| | | |___  | | \  | | |  ___| |   / /     ___| |
+ * \_____/ |_____| |_|  \_| |_| /_____/  /_/     /_____/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author iTX Technologies
+ * @link https://itxtech.org
+ *
+ */
+
+namespace pocketmine\level\particle;
+
+use pocketmine\math\Vector3;
+
+class WhiteSmokeParticle extends GenericParticle{
+	public function __construct(Vector3 $pos, $scale = 0){
+		parent::__construct($pos, Particle::TYPE_WHITE_SMOKE, (int) $scale);
+	}
+}
