2431f3fbf6d22cbf3e47307867440af3ab072360
PeratX <1215714524@qq.com>
2016-07-06 09:03:51 +0800

    Revert "Fixed Arrow getMaxStackSize"
    This reverts commit 9675f079414792f8d67305ebf3ba5ed09d10b86e.

--- src/pocketmine/item/Arrow.php
+++ src/pocketmine/item/Arrow.php
@@ -25,8 +25,4 @@ class Arrow extends Item{
 	public function __construct($meta = 0, $count = 1) {
 		parent::__construct(self::ARROW, $meta, $count, "Arrow");
 	}
-
-	public function getMaxStackSize() : int{
-		return 1;
-	}
 }
