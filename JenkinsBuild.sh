93d5a614d013cae927da6fc6218a4db991249421
Tatsuyuki Ishi <ishitatsuyuki@gmail.com>
2016-07-15 13:05:48 +0900

    Fix Jenkins Build

--- JenkinsBuild.sh
+++ JenkinsBuild.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sed -i "s/const VERSION \?= \?\"\(.*\)\"/const VERSION = \"\1-${1}\"/" src/pocketmine/PocketMine.php
 echo -e "version\nms\nstop\n" | /opt/php/bin/php src/pocketmine/PocketMine.php --no-wizard
+mkdir -p /opt/data-2T/jenkins/jobs/Genisys-master/workspace/artifact/
 mv /opt/data-2T/jenkins/jobs/Genisys-master/workspace/plugins/Genisys/Genisys_*.phar /opt/data-2T/jenkins/jobs/Genisys-master/workspace/artifact/Genisys-${1}.phar
+exit $?
