c33e0109a7ecf9fc7cb249047a448d57abb66aeb
Tatsuyuki Ishi <ishitatsuyuki@gmail.com>
2016-07-15 12:48:14 +0900

    Fix VersionString for rolling release

--- src/pocketmine/utils/VersionString.php
+++ src/pocketmine/utils/VersionString.php
@@ -1,4 +1,4 @@
-<?php
+4<?php
 
 /*
  *
@@ -37,12 +37,11 @@ class VersionString{
 			$this->major = ($version >> 5) & 0x0F;
 			$this->generation = ($version >> 9) & 0x0F;
 		}else{
-			$version = preg_split("/([A-Za-z]*)[ _\\-]?([0-9]*)\\.([0-9]*)\\.{0,1}([0-9]*)(dev|)(-[\\0-9]{1,}|)/", $version, -1, PREG_SPLIT_DELIM_CAPTURE);
-			$this->generation = isset($version[2]) ? (int) $version[2] : 0; //0-15
-			$this->major = isset($version[3]) ? (int) $version[3] : 0; //0-15
-			$this->minor = isset($version[4]) ? (int) $version[4] : 0; //0-31
-			$this->development = $version[5] === "dev" ? true : false;
-			if($version[6] !== ""){
+			$this->generation = 0;
+			$this->major = 0;
+			$this->minor = 0;
+			$this->development = true;
+			if($version[5] !== ""){
 				$this->build = intval(substr($version[6], 1));
 			}else{
 				$this->build = 0;
