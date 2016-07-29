6326c2e62f3d2c28d373bfd31371bc94709d9412
dog194 <514896768@qq.com>
2016-07-29 23:32:01 +0800

    Fix a bug
    Fix a bug

--- src/pocketmine/block/Block.php
+++ src/pocketmine/block/Block.php
@@ -320,14 +320,16 @@ class Block extends Position implements BlockIds, Metadatable{
 					self::$hardness[$id] = $block->getHardness();
 					self::$light[$id] = $block->getLightLevel();
 
-					if($block->isTransparent()){
-						if($block instanceof Water or $block instanceof Ice){
-							self::$lightFilter[$id] = 2;
-						}else{
-							self::$lightFilter[$id] = 1;
-						}
-					}elseif($block->isSolid()){
-						if($block instanceof SolidLight){
+					if($block->isSolid()){
+						if($block->isTransparent()){
+							if($block instanceof Lava){
+								self::$lightFilter[$id] = 15;
+							}elseif($block instanceof Liquid or $block instanceof Ice){
+								self::$lightFilter[$id] = 2;
+							}else{
+								self::$lightFilter[$id] = 1;
+							}
+						}elseif($block instanceof SolidLight){
 							self::$lightFilter[$id] = 1;
 						}else{
 							self::$lightFilter[$id] = 15;
