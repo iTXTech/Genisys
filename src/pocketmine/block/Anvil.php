30a8d404374b70fec4d0952ed69a183e853c12e1
dktapps <odigiman@gmail.com>
2016-07-30 17:51:02 +0100

    Anvil: fix rotation and drops

--- src/pocketmine/block/Anvil.php
+++ src/pocketmine/block/Anvil.php
@@ -79,14 +79,16 @@ class Anvil extends Fallable{
 	}
 	
 	public function place(Item $item, Block $block, Block $target, $face, $fx, $fy, $fz, Player $player = null){
-		parent::place($item, $block, $target, $face, $fx, $fy, $fz, $player);
+		$direction = ($player !== null? $player->getDirection(): 0) & 0x03;
+		$this->meta = ($this->meta & 0x0c) | $direction;
+		$this->getLevel()->setBlock($block, $this, true, true);
 		$this->getLevel()->addSound(new AnvilFallSound($this));
 	}
 
 	public function getDrops(Item $item) : array {
 		if($item->isPickaxe() >= 1){
 			return [
-				[$this->id, 0, 1], //TODO break level
+				[$this->id, $this->meta & 0x0c, 1],
 			];
 		}else{
 			return [];
