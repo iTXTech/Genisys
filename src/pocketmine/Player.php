927deb00c2fbe303002b24ecdf13395d5aa2b6d7
Dylan K. Taylor <odigiman@gmail.com>
2016-08-16 11:26:59 +0100

    Fix plugin crashes

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -2370,6 +2370,7 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 
 				if($this->server->getConfigBoolean("online-mode", false) && $packet->identityPublicKey === null){
 					$this->kick("disconnectionScreen.notAuthenticated", false);
+					break;
 				}
 
 				if(count($this->server->getOnlinePlayers()) >= $this->server->getMaxPlayers() and $this->kick("disconnectionScreen.serverFull", false)){
