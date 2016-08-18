79d49fdf5ccca417e9a6f3e8cc2eeb1ee67ea5e1
Tarik02 <Taras.Fomin@gmail.com>
2016-08-18 16:13:12 +0300

    Level::dropItem now returns the dropped item

--- src/pocketmine/level/Level.php
+++ src/pocketmine/level/Level.php
@@ -1566,6 +1566,8 @@ class Level implements ChunkManager, Metadatable{
 	 * @param Item    $item
 	 * @param Vector3 $motion
 	 * @param int     $delay
+	 *
+	 * @return null|DroppedItem
 	 */
 	public function dropItem(Vector3 $source, Item $item, Vector3 $motion = null, $delay = 10){
 		$motion = $motion === null ? new Vector3(lcg_value() * 0.2 - 0.1, 0.2, lcg_value() * 0.2 - 0.1) : $motion;
@@ -1595,7 +1597,11 @@ class Level implements ChunkManager, Metadatable{
 			]));
 
 			$itemEntity->spawnToAll();
+
+			return $itemEntity;
 		}
+
+		return null;
 	}
 
 	/**
