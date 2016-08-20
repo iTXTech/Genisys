1bbecbd64ef1b484861752197542c36a6fd1bbe1
dktapps <odigiman@gmail.com>
2016-08-20 21:17:26 +0100

    Revert "Fix entity coordinate bugs" This reverts commit 1657965320ee353c4911a8d9f760bba0a0512811.
    seems this broke lots of things. Needs more tests.

--- src/pocketmine/entity/Entity.php
+++ src/pocketmine/entity/Entity.php
@@ -964,7 +964,7 @@ abstract class Entity extends Location implements Metadatable{
 			$this->lastYaw = $this->yaw;
 			$this->lastPitch = $this->pitch;
 
-			$this->addMovement($this->x, $this->y, $this->z, $this->yaw, $this->pitch, $this->yaw);
+			$this->addMovement($this->x, $this->y + $this->getEyeHeight(), $this->z, $this->yaw, $this->pitch, $this->yaw);
 		}
 
 		if($diffMotion > 0.0025 or ($diffMotion > 0.0001 and $this->getMotion()->lengthSquared() <= 0.0001)){ //0.05 ** 2
