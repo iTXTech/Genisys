cedc4a5e4987a2ee80f9b11fe0f265a6ed41ab4e
Dylan K. Taylor <odigiman@gmail.com>
2016-08-20 15:24:28 +0100

    Fix #1716

--- src/synapse/network/protocol/spp/FastPlayerListPacket.php
+++ src/synapse/network/protocol/spp/FastPlayerListPacket.php
@@ -26,7 +26,7 @@ class FastPlayerListPacket extends DataPacket{
 
 	public function encode(){
 		$this->reset();
-		$this->putUUID($sendTo);
+		$this->putUUID($this->sendTo);
 		$this->putByte($this->type);
 		$this->putInt(count($this->entries));
 		foreach($this->entries as $d){
