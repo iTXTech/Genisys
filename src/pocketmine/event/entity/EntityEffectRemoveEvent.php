7b76ced51c8fc5335b97e27637b722eb26aaa533
sero583 <serhatguler@hotmail.de>
2016-08-21 15:33:38 +0200

    Update EntityEffectRemoveEvent.php
    New getter, called getEffect();

--- src/pocketmine/event/entity/EntityEffectRemoveEvent.php
+++ src/pocketmine/event/entity/EntityEffectRemoveEvent.php
@@ -27,12 +27,20 @@ use pocketmine\event\Cancellable;
 use pocketmine\entity\Effect;
 
 class EntityEffectRemoveEvent extends EntityEvent implements Cancellable{
-
 	public static $handlerList = null;
+	
+	/** @var Effect */
+	private $effect;
 
 	public function __construct(Entity $entity, int $effect){
 		$this->entity = $entity;
 		$this->effect = $effect;
 	}
 
+	/**
+	 * @return Effect
+	 */
+	public function getEffect() {
+		return $this->effect;
+	}
 }
