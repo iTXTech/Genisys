4d80965c837d1ac65be130c6404914d89247c7d0
NycuR0 <nghergu14@gmail.com>
2016-08-28 18:50:56 +0300

    Update FastPlayerListPacket.php

--- src/synapse/network/protocol/spp/FastPlayerListPacket.php
+++ src/synapse/network/protocol/spp/FastPlayerListPacket.php
@@ -2,7 +2,7 @@
 
 namespace synapse\network\protocol\spp;
 
-use src/pocketmine/utils/BinaryStream;
+use pocketmine/utils/BinaryStream;
 
 class FastPlayerListPacket extends DataPacket{
 	const NETWORK_ID = Info::FAST_PLAYER_LIST_PACKET;
