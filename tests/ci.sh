8e139ea9b09a4efc4e5d583797adc2b1a976be92
Dylan K. Taylor <odigiman@gmail.com>
2016-09-04 15:06:30 +0100

    CI: Use new DevTools

--- tests/ci.sh
+++ tests/ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir plugins
-curl -fsSL https://github.com/iTXTech/Genisys-DevTools/releases/download/1.0.0/Genisys-DevTools_v1.0.0.phar -o plugins/Genisys-DevTools.phar
+curl -fsSL https://github.com/iTXTech/DevTools/releases/download/v1.1-iTX/DevTools_v1.1-iTX.phar -o plugins/DevTools.phar
 echo Running lint...
 shopt -s globstar
 for file in **/*.php; do
@@ -9,7 +9,7 @@ for file in **/*.php; do
 done
 echo Lint done successfully.
 echo -e "version\nms\nstop\n" | php src/pocketmine/PocketMine.php --no-wizard | grep -v "\[DevTools\] Adding "
-if ls plugins/Genisys-DevTools_OUTPUT/Genisys*.phar >/dev/null 2>&1; then
+if ls plugins/DevTools_OUTPUT/Genisys*.phar >/dev/null 2>&1; then
     echo Server packaged successfully.
 else
     echo No phar created!
