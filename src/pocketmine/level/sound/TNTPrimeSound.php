786112b828bac072f6760a65a52f6551036694e4
PeratX <1215714524@qq.com>
2016-09-04 15:14:39 +0800

    Format: Formatted codes

--- src/pocketmine/level/sound/TNTPrimeSound.php
+++ src/pocketmine/level/sound/TNTPrimeSound.php
@@ -1,9 +1,23 @@
 <?php
 
-/**
- * Author: Pub4Game
- * OpenGenisys Project
-*/
+/*
+ *
+ *  _____   _____   __   _   _   _____  __    __  _____
+ * /  ___| | ____| |  \ | | | | /  ___/ \ \  / / /  ___/
+ * | |     | |__   |   \| | | | | |___   \ \/ /  | |___
+ * | |  _  |  __|  | |\   | | | \___  \   \  /   \___  \
+ * | |_| | | |___  | | \  | | |  ___| |   / /     ___| |
+ * \_____/ |_____| |_|  \_| |_| /_____/  /_/     /_____/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author iTX Technologies
+ * @link https://itxtech.org
+ *
+ */
 
 namespace pocketmine\level\sound;
 
