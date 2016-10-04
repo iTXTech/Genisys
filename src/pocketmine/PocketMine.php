50d5f3855ac60386e8299b9d9b48290e5ea0e602
Dylan K. Taylor <odigiman@gmail.com>
2016-10-04 21:54:21 +0100

    AdventureSettings fixes and added isFlying properties and methods

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -73,7 +73,7 @@ namespace pocketmine {
 	use pocketmine\wizard\Installer;
 
 	const VERSION = ""; //will be set by CI to a git hash
-	const API_VERSION = "2.0.0";
+	const API_VERSION = "2.0.1";
 	const CODENAME = "Kyrios";
 	const MINECRAFT_VERSION = "v0.15.90.7 alpha";
 	const MINECRAFT_VERSION_NETWORK = "0.15.90.7";
