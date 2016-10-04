ba6ba92b390db8923dde4b293f8c371255f6fe97
Fury <xfurymcpe@gmail.com>
2016-10-04 16:19:18 -0400

    Fix PlayerDropItemEvent cancelling (ATTEMPT 2)

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -3581,6 +3581,8 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 		$ev = new PlayerDropItemEvent($this, $item);
 		$this->server->getPluginManager()->callEvent($ev);
 		if($ev->isCancelled()){
+			$p->getFloatingInventory()->removeItem($item);
+			$p->getInventory()->addItem($item);
 			return;
 		}
 
