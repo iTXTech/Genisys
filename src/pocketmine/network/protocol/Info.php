23fc576668602ec78347c60a7c054ecc81ff8489
Harvey Hans <harveyhansjavier@gmail.com>
2016-10-19 17:14:10 +0800

    Added 0.15.9 Support

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -31,7 +31,7 @@ interface Info{
 	 * Actual Minecraft: PE protocol version
 	 */
 	const CURRENT_PROTOCOL = 84;
-	const ACCEPTED_PROTOCOLS = [84];
+	const ACCEPTED_PROTOCOLS = [83, 84];
 
 	const LOGIN_PACKET = 0x01;
 	const PLAY_STATUS_PACKET = 0x02;
