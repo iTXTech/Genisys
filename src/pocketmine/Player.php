5a0110636daeb7e274cead4db7aa865ef3219766
Dylan K. Taylor <odigiman@gmail.com>
2016-11-22 16:55:40 +0000

    Ignore MovePlayerPacket if position and rotation is the same

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -2360,6 +2360,10 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 
 				$newPos = new Vector3($packet->x, $packet->y - $this->getEyeHeight(), $packet->z);
 
+				if($newPos->distanceSquared($this) < 0.01 and ($packet->yaw % 360) === $this->yaw and ($packet->pitch % 360) === $this->pitch){ //player hasn't moved, just client spamming packets
+					break;
+				}
+
 				$revert = false;
 				if(!$this->isAlive() or $this->spawned !== true){
 					$revert = true;
