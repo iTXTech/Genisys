4a61667cb3bf38d1ff4f7f9d1e6e27a95c3ad2ac
laurobeleche <laurobeleche@gmail.com>
2016-11-27 14:51:45 -0200

    Fix #2218 (#2231)

--- src/pocketmine/inventory/PlayerInventory.php
+++ src/pocketmine/inventory/PlayerInventory.php
@@ -277,6 +277,14 @@ class PlayerInventory extends BaseInventory{
 		return $this->setItem($this->getSize() + $index, $item);
 	}
 
+	public function damageArmor($index, $cost){
+		$this->slots[$this->getSize() + $index]->useOn($this->slots[$this->getSize() + $index], $cost);
+		if($this->slots[$this->getSize() + $index]->getDamage() >= $this->slots[$this->getSize() + $index]->getMaxDurability()){
+			$this->setItem($this->getSize() + $index, Item::get(Item::AIR, 0, 0));
+		}
+		$this->sendArmorContents($this->getViewers());
+	}
+
 	public function getHelmet(){
 		return $this->getItem($this->getSize());
 	}
