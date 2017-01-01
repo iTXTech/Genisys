6d88a18b42fc925bb51f7ed41577449f1c927b7e
Dylan K. Taylor <odigiman@gmail.com>
2017-01-01 12:57:09 +0000

    Removed the need for subchunks to know their Y coordinates

--- src/pocketmine/level/format/region/McRegion.php
+++ src/pocketmine/level/format/region/McRegion.php
@@ -69,10 +69,11 @@ class McRegion extends BaseLevelProvider{
 		$data = "";
 		$blockLight = "";
 		$skyLight = "";
+		$subChunks = $chunk->getSubChunks();
 		for($x = 0; $x < 16; ++$x){
 			for($z = 0; $z < 16; ++$z){
 				for($y = 0; $y < 8; ++$y){
-					$subChunk = $chunk->getSubChunk($y);
+					$subChunk = $subChunks[$y];
 					$ids .= $subChunk->getBlockIdColumn($x, $z);
 					$data .= $subChunk->getBlockDataColumn($x, $z);
 					$blockLight .= $subChunk->getBlockLightColumn($x, $z);
@@ -168,7 +169,7 @@ class McRegion extends BaseLevelProvider{
 					$skyLight .= substr($fullSkyLight, $offset, 8);
 					$offset += 64;
 				}
-				$subChunks[] = new SubChunk($y, $ids, $data, $blockLight, $skyLight);
+				$subChunks[$y] = new SubChunk($ids, $data, $blockLight, $skyLight);
 			}
 
 			if(isset($chunk->BiomeColors)){
