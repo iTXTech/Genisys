dschwartz783/PocketMine-MP-1

Workaround for glob() fail, fix worlds not loading properly on some systems, close #197

src/pocketmine/level/format/region/McRegion.php
@@ -210,6 +210,12 @@ class McRegion extends BaseLevelProvider{
 
 		if($isValid){
 			$files = glob($path . "/region/*.mc*");
+			if(empty($files)){ //possible glob() issue on some systems
+				$files = array_filter(scandir($path . "/region/"), function($file){
+					return substr($file, strrpos($f, ".") + 1, 2) === "mc"; //region file
+				});
+			}
+
 			foreach($files as $f){
 				if(substr($f, strrpos($f, ".") + 1) !== static::REGION_FILE_EXTENSION){
 					$isValid = false;

iTXTech/Genisys

src/pocketmine/level/format/region/McRegion.php
@@ -213,6 +213,12 @@ class McRegion extends BaseLevelProvider{
 
 		if($isValid){
 			$files = glob($path . "/region/*.mc*");
+			if(empty($files)){ //possible glob() issue on some systems
+				$files = array_filter(scandir($path . "/region/"), function($file){
+					return substr($file, strrpos($file, ".") + 1, 2) === "mc"; //region file
+				});
+			}
+
 			foreach($files as $f){
 				if(substr($f, strrpos($f, ".") + 1) !== static::REGION_FILE_EXTENSION){
 					$isValid = false;