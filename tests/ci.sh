6f7391bfe2483ab5588891e84946354ce597faf5
Dylan K. Taylor <odigiman@gmail.com>
2017-01-09 11:48:17 +0000

    Fix Travis

--- tests/ci.sh
+++ tests/ci.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 mkdir plugins
-curl -fsSL https://github.com/iTXTech/DevTools/releases/download/v1.1-iTX/DevTools_v1.1-iTX.phar -o plugins/DevTools.phar
+curl -o DevTools.zip "https://jenkins.pmmp.io/job/PocketMine-MP%20DevTools/lastSuccessfulBuild/artifact/*zip*/artifacts.zip"
+unzip DevTools.zip
+mv archive/*.phar plugins
 echo Running lint...
 shopt -s globstar
 for file in src/pocketmine/*.php src/pocketmine/**/*.php; do
@@ -8,8 +10,8 @@ for file in src/pocketmine/*.php src/pocketmine/**/*.php; do
     [ $? -ne 0 ] && echo -n "$OUTPUT" && exit 1
 done
 echo Lint done successfully.
-echo -e "version\nms\nstop\n" | php src/pocketmine/PocketMine.php --no-wizard --disable-readline --debug.level=2 | grep -v "\[DevTools\] Adding "
-if ls plugins/DevTools_OUTPUT/Genisys*.phar >/dev/null 2>&1; then
+echo -e "version\nmakeserver\nstop\n" | php src/pocketmine/PocketMine.php --no-wizard --disable-readline --debug.level=2 | grep -v "\[DevTools\] Adding "
+if ls plugins/DevTools/Genisys*.phar >/dev/null 2>&1; then
     echo Server packaged successfully.
 else
     echo No phar created!
