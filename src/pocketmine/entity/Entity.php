b66c0985ee40bda48b83bab9f6e07549c30f8c31
Erkam Kahraman <erkamkahriman@gmail.com>
2017-01-20 17:08:13 +0100

    ...

--- src/pocketmine/entity/Entity.php
+++ src/pocketmine/entity/Entity.php
@@ -1807,7 +1807,7 @@ abstract class Entity extends Location implements Metadatable{
 			}
 		}
 
-		if($this->getLevel()->getServer()->redstoneEnabled){
+		if($this->getServer()->getLevel->redstoneEnabled){
 			/** @var \pocketmine\block\PressurePlate $block * */
 			foreach($this->activatedPressurePlates as $key => $block){
 				$block->checkActivation();
