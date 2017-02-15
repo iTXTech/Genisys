995105bc0b817446ce471215aaf0d4963d2a7338
Dylan K. Taylor <odigiman@gmail.com>
2017-02-15 10:37:05 +0000

    Added Inventory network IDs interface

--- src/pocketmine/inventory/InventoryType.php
+++ src/pocketmine/inventory/InventoryType.php
@@ -21,10 +21,14 @@
 
 namespace pocketmine\inventory;
 
+use pocketmine\network\protocol\types\InventoryNetworkIds;
+
 /**
  * Saves all the information regarding default inventory sizes and types
  */
 class InventoryType{
+
+	//NOTE: Do not confuse these with the network IDs.
 	const CHEST = 0;
 	const DOUBLE_CHEST = 1;
 	const PLAYER = 2;
@@ -62,21 +66,25 @@ class InventoryType{
 			return;
 		}
 
-		static::$default[static::CHEST] = new InventoryType(27, "Chest", 0);
-		static::$default[static::DOUBLE_CHEST] = new InventoryType(27 + 27, "Double Chest", 0);
-		static::$default[static::PLAYER] = new InventoryType(36 + 4, "Player", 0); //36 CONTAINER, 4 ARMOR
-		static::$default[static::FURNACE] = new InventoryType(3, "Furnace", 2);
-		static::$default[static::CRAFTING] = new InventoryType(5, "Crafting", 1); //4 CRAFTING slots, 1 RESULT
-		static::$default[static::WORKBENCH] = new InventoryType(10, "Crafting", 1); //9 CRAFTING slots, 1 RESULT
-		static::$default[static::ENCHANT_TABLE] = new InventoryType(2, "Enchant", 3); //1 INPUT/OUTPUT, 1 LAPIS
-		static::$default[static::BREWING_STAND] = new InventoryType(4, "Brewing", 4); //1 INPUT, 3 POTION
-		static::$default[static::ANVIL] = new InventoryType(3, "Anvil", 5); //2 INPUT, 1 OUTPUT
-		static::$default[static::DISPENSER] = new InventoryType(9, "Dispenser", 6); //9 CONTAINER
-		static::$default[static::DROPPER] = new InventoryType(9, "Dropper", 7); //9 CONTAINER
-		static::$default[static::HOPPER] = new InventoryType(5, "Hopper", 8); //5 CONTAINER
-		static::$default[static::ENDER_CHEST] = new InventoryType(27, "Ender Chest", 0);
+		//TODO: move network stuff out of here
+		//TODO: move inventory data to json
+		static::$default = [
+			static::CHEST =>         new InventoryType(27,      "Chest",        InventoryNetworkIds::CONTAINER),
+			static::DOUBLE_CHEST =>  new InventoryType(27 + 27, "Double Chest", InventoryNetworkIds::CONTAINER),
+			static::PLAYER =>        new InventoryType(36 + 4,  "Player",       InventoryNetworkIds::INVENTORY), //36 CONTAINER, 4 ARMOR
+			static::CRAFTING =>      new InventoryType(5,       "Crafting",     InventoryNetworkIds::INVENTORY), //yes, the use of INVENTORY is intended! 4 CRAFTING slots, 1 RESULT
+			static::WORKBENCH =>     new InventoryType(10,      "Crafting",     InventoryNetworkIds::WORKBENCH), //9 CRAFTING slots, 1 RESULT
+			static::FURNACE =>       new InventoryType(3,       "Furnace",      InventoryNetworkIds::FURNACE), //2 INPUT, 1 OUTPUT
+			static::ENCHANT_TABLE => new InventoryType(2,       "Enchant",      InventoryNetworkIds::ENCHANTMENT), //1 INPUT/OUTPUT, 1 LAPIS
+			static::BREWING_STAND => new InventoryType(4,       "Brewing",      InventoryNetworkIds::BREWING_STAND), //1 INPUT, 3 POTION
+			static::ANVIL =>         new InventoryType(3,       "Anvil",        InventoryNetworkIds::ANVIL), //2 INPUT, 1 OUTPUT
+			static::DISPENSER =>     new InventoryType(9,       "Dispenser",    InventoryNetworkIds::DISPENSER), //9 CONTAINER
+			static::DROPPER =>       new InventoryType(9,       "Dropper",      InventoryNetworkIds::DROPPER), //9 CONTAINER
+			static::HOPPER =>        new InventoryType(5,       "Hopper",       InventoryNetworkIds::HOPPER), //5 CONTAINER
+			static::ENDER_CHEST =>   new InventoryType(27,      "Ender Chest",  InventoryNetworkIds::CONTAINER),
 
-		static::$default[static::PLAYER_FLOATING] = new InventoryType(36, "Floating", null); //Mirror all slots of main inventory (needed for large item pickups)
+			static::PLAYER_FLOATING => new InventoryType(36, "Floating", null) //Mirror all slots of main inventory (needed for large item pickups)
+		];
 	}
 
 	/**
