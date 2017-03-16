ee1f0d083d60475931ea135583ab75da9ad21c49
Tatsuyuki Ishi <ishitatsuyuki@gmail.com>
2017-03-16 11:54:51 +0900

    Dockerfile: add entrypoint

---
+++ ci/prod/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "${1:0:1}" = '-' ]; then
+	set -- php /Genisys.phar "$@"
+fi
+
+# allow the container to be started with `--user`
+if [ "$1" = 'php' ] && [ "$2" = '/Genisys.phar' ] && [ "$(id -u)" = '0' ]; then
+	chown -R genisys .
+	exec gosu genisys "$BASH_SOURCE" "$@"
+fi
+
+exec "$@"
