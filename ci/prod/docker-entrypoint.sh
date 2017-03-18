0595a92bafc2fa9020eb1922108740f76b4c4be4
Tatsuyuki Ishi <ishitatsuyuki@gmail.com>
2017-03-18 18:33:17 +0900

    Change entrypoint to be sh compatible

--- ci/prod/docker-entrypoint.sh
+++ ci/prod/docker-entrypoint.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ "${1:0:1}" = '-' ]; then
+if [ "${1#-}" != "$1" ]; then
 	set -- php /Genisys.phar "$@"
 fi
 
 # allow the container to be started with `--user`
 if [ "$1" = 'php' ] && [ "$2" = '/Genisys.phar' ] && [ "$(id -u)" = '0' ]; then
 	chown -R genisys .
-	exec su-exec genisys "$BASH_SOURCE" "$@"
+	exec su-exec genisys "$0" "$@"
 fi
 
 exec "$@"
